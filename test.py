178b4ff76b23205185cca399f047fd218fe89209
John Roller <jtroller33@gmail.com>
2022-12-30 10:57:31 -0600

    num

--- test.py
+++ test.py
@@ -136,3 +136,9 @@ def if_stmt():
     x = 42
     if x > 5:
         print("x > 5")
+        
+    num = 12
+    if num > 5:
+        print("Bigger than 5")
+        if num <= 47:
+            print("between 5 and 47")
