f3b965f7c19c563076103ecab264148bfe4d4b4b
John Roller <jtroller33@gmail.com>
2022-12-31 00:49:01 -0600

    nested_ifs

--- test.py
+++ test.py
@@ -178,3 +178,10 @@ def else_stmt():
     else:
         print("no")
         
+def nested_ifs():
+    if 1 + 1 == 2:
+        if 2 * 2 == 8:
+            print("if")
+        else:
+            print("else")
+        
