15f287f550537f015c8beef9c9013f25351ff08f
John Roller <jtroller33@gmail.com>
2022-12-31 22:17:03 -0600

    1107

--- test.py
+++ test.py
@@ -287,7 +287,3 @@ def how_many_nums():
 # Should old acquaintance be forgot
 # In the days of auld lang syne?
 
-# For auld lang syne, my dear
-# For auld lang syne
-# We'll drink a cup of kindness yet
-# For the sake of auld lang syne
