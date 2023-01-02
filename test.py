6311696fb1b11a1034b4d214857e136d4f8e98ca
John Roller <jtroller33@gmail.com>
2023-01-01 22:59:13 -0600

    gitignore

--- test.py
+++ test.py
@@ -285,5 +285,13 @@ def breaks():
             break
     
     print("finished")
-        
+
+def continues():
+    i = 0
+    while i < 5:
+        i += 1
+        if i == 3:
+            print("Skipping 3")
+            continue
+        print(i)
         
