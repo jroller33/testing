d74f67b590a8364ddbb6e8424e4f9946c3ce8c06
John Roller <jtroller33@gmail.com>
2023-01-16 20:19:03 -0600

    def placehldr

--- test.py
+++ test.py
@@ -529,5 +529,13 @@ def format_strings():
     nums = [4, 5, 6]
     msg = "Numbers: {0} {1} {2}". format(nums[0], nums[1], nums[2])
     print(msg)
+    # make sure the numbers in {} match the order of the args in format()
+    
+def name_placehldr():
+    a = "{x}, {y}".format(x=5, y=12)
+    print(a)\
+
+def join_str():
+    
     
 
